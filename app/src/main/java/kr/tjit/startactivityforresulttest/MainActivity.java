2b01c1467e3f24e79c0e17954d6fc81c0adeb9a1
manymany.macarons <manymany.macarons@gmail.com>
2018-12-29 16:21:28 +0900

    카메라 권한 설정

--- app/src/main/java/kr/tjit/startactivityforresulttest/MainActivity.java
+++ app/src/main/java/kr/tjit/startactivityforresulttest/MainActivity.java
@@ -1,8 +1,10 @@
 package kr.tjit.startactivityforresulttest;
 
+import android.Manifest;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.Button;
@@ -11,6 +13,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+
+import java.util.List;
 
 public class MainActivity extends BaseActivity {
 
@@ -18,6 +24,7 @@ public class MainActivity extends BaseActivity {
     final static int REQUEST_FOR_USER_BIRTHDAY = 1001;
     final static int REQUEST_FOR_PICTURE_GALLERY = 1002;
     final static int REQUEST_FOR_PICTURE_CROP = 1003;
+    final static int REQUEST_FOR_PICTURE_CAMERA = 1004;
 
     private android.widget.TextView nameTxt;
     private android.widget.Button nameInputBtn;
@@ -54,6 +61,7 @@ public class MainActivity extends BaseActivity {
             Toast.makeText(mContext, "생년월일", Toast.LENGTH_SHORT).show();
         }
         else if (requestCode == REQUEST_FOR_PICTURE_GALLERY) {
+//            갤러리의 사진을 가져온 경우
             if (resultCode == RESULT_OK) {
                 Uri selectedImageUri = data.getData();
 //                String imagePath = selectedImageUri.getPath();
@@ -94,6 +102,28 @@ public class MainActivity extends BaseActivity {
         cameraBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                PermissionListener permissionListener = new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted() {
+//                        허가가 난 경우 카메라 화면 실행
+                        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        startActivityForResult(intent, REQUEST_FOR_PICTURE_CAMERA);
+                    }
+
+                    @Override
+                    public void onPermissionDenied(List<String> deniedPermissions) {
+//                        허가가 안나면 권한이 필요하다는 토스트 출력
+                        Toast.makeText(mContext, "카메라 권한이 필요합니다.", Toast.LENGTH_SHORT).show();
+
+                    }
+//                    세미콜론 찍어서 문장 마무리해줘야 함!
+                };
+
+                TedPermission.with(mContext)
+                        .setPermissionListener(permissionListener)
+                        .setPermissions(Manifest.permission.CAMERA)
+                        .check();
 
             }
         });
